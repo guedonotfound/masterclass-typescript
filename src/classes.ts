328881850e401a6f15d12df543d0519689eec2f9
Guedo <g.guedes.a@hotmail.com>
2025-05-27 11:48:34 -0300

    feat: generics concept

---
+++ src/classes.ts
@@ -0,0 +1,33 @@
+interface IPerson {
+  id: number;
+  sayMyName(): string;
+}
+
+class Person implements IPerson {
+  readonly id: number;
+  protected name: string;
+  private age: number;
+
+  constructor(id: number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  sayMyName(): string {
+    return this.name;
+  }
+}
+
+class Employee extends Person {
+  constructor(id: number, name: string, age: number) {
+    super(id, name, age);
+  }
+
+  whoAmI() {
+    return this.name;
+  }
+}
+
+const gabriel = new Person(1, "Gabriel", 27);
+// gabriel.id = 32;
