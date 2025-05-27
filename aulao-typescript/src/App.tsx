7c66c97b2ec347b7db41a5c540eefe2368fab4ec
Guedo <g.guedes.a@hotmail.com>
2025-05-27 12:03:13 -0300

    feat: typescript w/ react intro

---
+++ aulao-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "./Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="teste" />
+    </div>
+  );
+}
+
+export default App;
